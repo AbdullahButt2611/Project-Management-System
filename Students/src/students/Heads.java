6bb8e6962fac052cf243a844c2416bcd0dd16e0a
AbdullahButt2611 <abutt2210@gmail.com>
2021-04-13 01:02:16 +0500

    Some more classes work
    Now i have prepared the more classes needed for my project along with their constructors and setter and getter

--- Students/src/students/Heads.java
+++ Students/src/students/Heads.java
@@ -38,6 +38,8 @@ public class Heads extends Details{
         super(gender);
         this.qualification=qualification;
         this.setID();
+        this.mark1=0;
+        this.mark2=0;
     }
 
     /**
@@ -132,5 +134,144 @@ public class Heads extends Details{
         return flag;
     }
     
+    /**
+     * This is a setter for the address of the person that is actually the group head
+     * It should be only alphanumeric
+     * @param password String inputted by the Student 
+     * @return Boolean showing whether the String is a valid address or not
+     */
+    public boolean setPassword(String password)
+    {
+        boolean flag=false;
+        int size=password.length();
+        for(int i=0;i<size;i++)
+        {
+            if((password.charAt(i)>='A' && password.charAt(i)<='Z')||(password.charAt(i)>='a' && password.charAt(i)<='z')||(password.charAt(i)>='0' && password.charAt(i)<='9'))
+            {
+                flag=true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        this.password=password;
+        return flag;
+    }
     
+    /**
+     * This is a setter for the marks of the first module that will be entered by the committee member 
+     * after evaluating the project
+     * @param marks String marks inputted by the user
+     * @return Boolean telling whether marks are valid or not
+     */
+    public boolean setMarks1(String marks)
+    {
+        boolean flag=false;
+        int size=marks.length();
+        for(int i=0;i<size;i++)
+        {
+            if(marks.charAt(i)>='0' && marks.charAt(i)<='9')
+            {
+                flag=true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        int mark=Integer.parseInt(marks);
+        if(mark>0 && mark<=25)
+        {
+            flag=true;
+        }
+        else
+        {
+            return false;
+        }
+        this.mark1=mark;
+        return flag;
+    }
+    
+    /**
+     * This is a setter for the marks of the second module that will be entered by the committee member 
+     * after evaluating the project
+     * @param marks String marks inputted by the user
+     * @return Boolean showing whether the String is a valid address or not
+     */
+    public boolean setMarks2(String marks)
+    {
+        boolean flag=false;
+        int size=marks.length();
+        for(int i=0;i<size;i++)
+        {
+            if(marks.charAt(i)>='0' && marks.charAt(i)<='9')
+            {
+                flag=true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        int mark=Integer.parseInt(marks);
+        if(mark>0 && mark<=25)
+        {
+            flag=true;
+        }
+        else
+        {
+            return false;
+        }
+        this.mark2=mark;
+        return flag;
+    }
+    
+    /**
+     * This is a setter for the marks of the third module that will be entered by the committee member 
+     * after evaluating the project
+     * @param marks String marks inputted by the user
+     * @return Boolean showing whether the String is a valid address or not
+     */
+    public boolean setMarks3(String marks)
+    {
+        boolean flag=false;
+        int size=marks.length();
+        for(int i=0;i<size;i++)
+        {
+            if(marks.charAt(i)>='0' && marks.charAt(i)<='9')
+            {
+                flag=true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        int mark=Integer.parseInt(marks);
+        if(mark>0 && mark<=50)
+        {
+            flag=true;
+        }
+        else
+        {
+            return false;
+        }
+        this.mark3=mark;
+        return flag;
+    }
+    
+    /**
+     * This is a method for calculating the total marks evaluated to the project out of 100
+     * @return int returning total marks given to the project work
+     */
+    public int getTotal()
+    {
+        int total=this.mark1+this.mark2+this.mark3;
+        return total;
+    }
 }
+
+
+
+    
--- Students/src/students/Students.java
+++ Students/src/students/Students.java
@@ -9,18 +9,65 @@ package students;
  *
  * @author DEll
  */
-public class Students {
+public class Students extends Details{
     
+    private String city;
     
+    private String qualification;
     
+    /**
+     * This is a constructor for the class of the Students 
+     * This is a parameterized constructor from which one is going to be the parameter for the
+     * parent class constructor
+     * @param gender Parameter for super class
+     * @param qualification String showing the qualification of the student
+     */
+    public Students(String gender, String qualification)
+    {
+        super(gender);
+        this.qualification=qualification;
+    }
 
     /**
-     * @param args the command line arguments
+     * This is a getter for the city in which the student resides
+     * @return String containing the name of the city
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        SignUp main=new SignUp();
-        main.setVisible(true);
+    public String getCity() {
+        return city;
     }
+
+    /**
+     * This is a getter for the qualification of the student
+     * @return String containing the qualification of the student
+     */
+    public String getQualification() {
+        return qualification;
+    }
+    
+    /**
+     * This is a setter for the city of the student where he resides
+     * @param city String inputted by the student
+     * @return Boolean telling whether the string is a valid City name or not
+     */
+    public boolean setCity(String city)
+    {
+        boolean flag=false;
+        int size=city.length();
+        for(int i=0;i<size;i++)
+        {
+            if((city.charAt(i)>='A' && city.charAt(i)<='Z')||(city.charAt(i)>='a' && city.charAt(i)<='z')||(city.charAt(i)>=' '))
+            {
+                flag=true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        this.city=city;
+        return flag;
+    }
+    
+
     
 }
